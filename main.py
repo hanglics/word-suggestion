72e2a988523f4526a9de7f26ce9f3b364ca7966c
Hang Li <cecillll.lee@gmail.com>
2019-12-03 15:52:11 +1000

    modify the api route from url to args

--- main.py
+++ main.py
@@ -6,13 +6,19 @@ from methods import *
 
 app = Flask(__name__)
 api = Api(app)
+app.config["DEBUG"] = True
 
 class getWordsSuggestions(Resource):
-    def get(self, word, size=10):
-        words = getWordSuggestions(word, int(size))
-        return jsonpify(words)
+    def get(self):
+        if 'term' in request.args and 'size' in request.args:
+            word = request.args["term"]
+            size = request.args["size"]
+            res = getWordSuggestions(word, int(size))
+            return jsonpify(res)
+        else:
+            return jsonpify([{"ERROR" : "No Term Or Size Provided."}])
     
-api.add_resource(getWordsSuggestions, '/words/<word>/<size>')
+api.add_resource(getWordsSuggestions, '/search')
 
 if __name__ == '__main__':
     print("Service runs on port: 6688")
